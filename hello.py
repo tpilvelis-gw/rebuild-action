85e08f6772e085114e9faaf3c4e1a01c58a94b5a
tpilvelis-gw <59439145+tpilvelis-gw@users.noreply.github.com>
2020-06-05 15:11:30 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,3 +2,6 @@ import sys
 
 sys.stdout.write("##[debug] Hello from python!")
 sys.stdout.write("##[debug]" + str(sys.argv[1:]))
+
+print("##[2debug] Hello from python!")
+print("##[2debug]" + str(sys.argv[1:]))
