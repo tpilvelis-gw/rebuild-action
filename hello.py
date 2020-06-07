5ff8c747790939a2e679725f9556647d2d3552b1
tpilvelis-gw <59439145+tpilvelis-gw@users.noreply.github.com>
2020-06-07 18:23:39 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,25 @@
 import sys
 import os
-from Glasswall import Glasswall
+import ctypes as ct
+
+# Glasswall Code
+class Glasswall:
+    """A Python API wrapper around the Glasswall library."""
+
+    gwLibrary = None
+    
+    def __init__(self, pathToLib):
+        """Constructor for the Glasswall library
+
+        :param str pathToLib: The file path to the Glasswall library.
+        """
+
+        try:
+            self.gwLibrary = ct.cdll.LoadLibrary(pathToLib)
+        except Exception as e:
+            raise Exception("Failed to load Glasswall library. Exception: {0}".format(e.message))
+        
+# Glasswall Code
 
 class Log:
     @staticmethod
