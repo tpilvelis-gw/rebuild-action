ad0f57a09388c3dbc64587e368ba6fa28f6583d7
tpilvelis-gw <59439145+tpilvelis-gw@users.noreply.github.com>
2020-06-08 08:17:18 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -113,14 +113,14 @@ def main():
 
     report1_h = "File"
     report2_h = "Status Code"
-    Log.report("|"+report1_h.ljust(50)+"|"+ report2_h.rjust(12)+"|")
+    Log.report("|"+report1_h.ljust(50)+"|"+ report2_h.rjust(15)+"|")
     for f in files_to_rebuild:
         a = datetime.datetime.now()
         protected_f = gw.GWFileProtect(f, args[3])
         b = datetime.datetime.now()
         delta = b - a
         
-        Log.report("|"+f.ljust(50)+"|"+ str(protected_f.returnStatus).rjust(12)+"|" + str(int(delta.min * 1000)) )
+        Log.report("|"+f.ljust(50)+"|"+ str(protected_f.returnStatus).rjust(15)+"|" + str(delta.microsecond() ) )
 
     Log.debug("Ending Script")
 
